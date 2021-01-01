0c4321a7a7283f6f35952509c881ed21890a2254
Bardini <derekbardini@gmail.com>
2020-12-31 21:40:56 -0600

    removed old comments and console logs.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -11,70 +10,45 @@ var yourPassword = ''
 function getPasswordCriteria() {
   var passwordLength = parseInt(
     prompt("How long would you like the password? (8-128 characters)")
-    );
+  );
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Bad password length!");
     return getPasswordCriteria();
-  } 
+  }
 
 
-var usingLowercase = confirm('Are we using lowercase letters?');
-var usingUppercase = confirm('Are we using uppercase letters?');
-var usingNumbers = confirm('Are we using numbers?');
-var usingSymbols = confirm('Are we using special characters?');
+  var usingLowercase = confirm('Are we using lowercase letters?');
+  var usingUppercase = confirm('Are we using uppercase letters?');
+  var usingNumbers = confirm('Are we using numbers?');
+  var usingSymbols = confirm('Are we using special characters?');
 
-if (usingLowercase) {
-  masterString += lowerCase;
-}
-if (usingUppercase) {
-  masterString += upperCase;
-}
-if (usingNumbers) {
-  masterString += numberStr;
-}
-if (usingSymbols) {
-  masterString += symbols;
+  if (usingLowercase) {
+    masterString += lowerCase;
+  }
+  if (usingUppercase) {
+    masterString += upperCase;
+  }
+  if (usingNumbers) {
+    masterString += numberStr;
+  }
+  if (usingSymbols) {
+    masterString += symbols;
   }
   function getRandomCharacter() {
-return masterString[Math.floor(Math.random()*masterString.length)];
+    return masterString[Math.floor(Math.random() * masterString.length)];
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+
+    yourPassword = yourPassword + getRandomCharacter();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = yourPassword;
+
+  }
 }
 
-for (let i = 0; i < passwordLength; i++) {
-  
- yourPassword = yourPassword + getRandomCharacter();
- var passwordText = document.querySelector("#password");
- passwordText.value = yourPassword;
-
-}
-console.log(masterString);
-console.log(passwordLength);
-console.log(getRandomCharacter());
-console.log(yourPassword);
-}
-
-
-
-
-
-
-
+// generateBtn.addEventListener("click", getPasswordCriteria);
 
 getPasswordCriteria();
 
-console.log(yourPassword);
 
-
-
-
-
-
-  // Write password to the #password input
-// function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  // passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
